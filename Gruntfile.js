b801dec44272867f129d327bdb54736f587a1e62
Jaime Pillora <jpillora@gmail.com>
2013-06-13 16:56:46 +1000

    added watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
 
+  var gruntdir = process.cwd();
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // ==================
@@ -22,8 +26,18 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
+    },
+    watch: {
+      dist: {
+        files: ['src/*.js'],
+        tasks: ['build'],
+        options: {
+          nospawn: true
+        }
+      }
     }
   });
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('build', ['uglify']);
+  grunt.registerTask('default', ['build', 'watch']);
 };
